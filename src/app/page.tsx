afa55c4b6058e0e32cfa2c8aede21b5c5f3b3711
lana.gordon <lana.gordon@slalom.com>
2024-08-01 14:09:48 +0100

    Homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,11 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Home() {
-	const posts = [
+	const pages = [
+		{
+			title: 'About',
+			slug: 'about',
+		},
 		{
 			title: 'Spatial',
 			slug: 'spatial',
@@ -11,13 +15,69 @@ export default function Home() {
 			title: 'Purpose',
 			slug: 'purpose',
 		},
+		{
+			title: 'Reset',
+			slug: 'reset',
+		},
+		{
+			title: 'Build',
+			slug: 'build',
+		},
+		{
+			title: 'Define',
+			slug: 'define',
+		},
+		{
+			title: 'Changing Faces',
+			slug: 'changing faces',
+		},
+		{
+			title: 'Crisis',
+			slug: 'crisis',
+		},
+		{
+			title: 'Girlyhood',
+			slug: 'girlyhood',
+		},
 	];
 
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<h1>Homepage</h1>
+		<main className="min-h-screen p-24 grid grid-cols-3">
+			<section className="col-span-2 px-12 ">
+				<div className="h-full flex flex-col justify-center gap-y-8">
+					<h2 className="text-sm font-garet uppercase tracking-wider">
+						Photography by
+					</h2>
+					<h1 className="text-7xl text-crayola-blue font-maragsa">
+						Lana Gordon
+					</h1>
+					<ul className="text-sm">
+						{pages.map((post) => (
+							<li
+								key={post.title}
+								className="inline-block mr-3 underline leading-6"
+							>
+								<Link href={`/${post.slug}`}>{post.title}</Link>
+							</li>
+						))}
+					</ul>
+				</div>
+			</section>
+			<section className="col-span-1">
+				{' '}
+				<div className=" w-full h-full relative">
+					<Link href="/spatial">
+						<Image
+							src="/photography/spatial/spatial-2.jpg"
+							alt="Image from the Spatial photoshoot"
+							fill={true}
+							objectFit="contain"
+						/>
+					</Link>
+				</div>
+			</section>
 
-			<div>
+			{/* <div>
 				<h2>Mapped List</h2>
 				<ul>
 					{posts.map((post) => (
@@ -26,12 +86,9 @@ export default function Home() {
 						</li>
 					))}
 				</ul>
-			</div>
-			<div>
-				<h2>Slug Link</h2>
-				<Link href={`/blog/a`}>blog link</Link>
-			</div>
-			<div>
+			</div> */}
+
+			{/* <div>
 				<Link href="/spatial">
 					<Image
 						className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
@@ -42,7 +99,7 @@ export default function Home() {
 						priority
 					/>
 				</Link>
-			</div>
+			</div> */}
 		</main>
 	);
 }
